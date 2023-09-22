bbaf5e6efa606f242b63dcd0ea4867f1b53ea9d5
Lucas Prieto <lucasprieto30@gmail.com>
2023-09-22 09:52:45 -0300

    Enable CORS endpoints

--- app/pruebas.py
+++ app/pruebas.py
@@ -1,15 +1,18 @@
 from flask_restx import Resource, Namespace, fields
 from .modelos import post_model
+from flask_cors import CORS
 
 ns = Namespace("Pruebas")
 
 @ns.route("/imagen")
+@cross_origin()
 class Pruebas(Resource):
     @ns.expect(post_model)
     def post(self):
         return {"saludo": "hola post"}, 200
     
 @ns.route("/")
+@cross_origin()
 class Pruebas(Resource):
     def get(self):
         return {"saludo": "hola get"}, 200
