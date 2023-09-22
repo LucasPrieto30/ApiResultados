a85bd0e96b1774c97801c6483ecf7a6259de9e78
Lucas Prieto <lucasprieto30@gmail.com>
2023-09-22 10:12:43 -0300

    fix cors

--- app/pruebas.py
+++ app/pruebas.py
@@ -5,14 +5,16 @@ from flask_cors import cross_origin
 ns = Namespace("Pruebas")
 
 @ns.route("/imagen")
-@cross_origin()
+
 class Pruebas(Resource):
     @ns.expect(post_model)
+    @cross_origin()
     def post(self):
         return {"saludo": "hola post"}, 200
     
 @ns.route("/")
 @cross_origin()
 class Pruebas(Resource):
+    @cross_origin()
     def get(self):
         return {"saludo": "hola get"}, 200
