30ef77ff86f230d82da7db39c47ae7acca243aea
Franco <francotorrico1999@gmail.com>
2023-10-01 01:01:33 -0300

    modificacion api rest

--- app/pruebas.py
+++ app/pruebas.py
@@ -1,5 +1,7 @@
 from flask_restx import Resource, Namespace, fields
 from .modelos import post_model
+#random
+import random
 
 ns = Namespace("Pruebas")
 
@@ -13,3 +15,21 @@ class Pruebas(Resource):
 class Pruebas(Resource):
     def get(self):
         return {"saludo": "hola get"}, 200
+    
+@ns.route("/predicciones")
+class PruebasPredicciones(Resource):
+    def get(self):
+        numeroPrediccion = round(random.uniform(0.00, 1.00), 2)
+        if numeroPrediccion < 0.60:
+            resultado = "No certero"
+        elif 0.60 <= numeroPrediccion < 0.65:
+            resultado = "Poco certero"
+        else:
+            resultado = "Muy certero"
+        numeroPrediccionFormateado = '{:.2f}'.format(numeroPrediccion)
+        print("el resultado ",numeroPrediccionFormateado,resultado)
+        respuesta = {
+            "precision": numeroPrediccionFormateado,
+            "prediccion": resultado
+        }
+        return respuesta, 200
