41cd6cdec236c2c80ab3e6cf695fa1742ea88b75
Lucas Prieto <lucasprieto30@gmail.com>
2023-10-18 13:58:40 -0300

    fix variables de entorno

--- database/db.py
+++ database/db.py
@@ -3,8 +3,8 @@ from psycopg2 import DatabaseError
 import os
 
 def get_connection():
-    password_bb = os.getenv(password_bb)
-    usuario_bb = os.getenv(usuario_bb)
+    password_bb = os.getenv('password_bb')
+    usuario_bb = os.getenv('usuario_bb')
     try:
         return psycopg2.connect(host='0.tcp.sa.ngrok.io',
                                 user=usuario_bb,
